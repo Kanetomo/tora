cc7adb9af3be1ca6ce10132c87476d5ead8cc258
Kanetomo <doldolkimali@gmail.com>
2020-12-01 03:22:32 +0900

    hooksに置き換え

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import LikeButton from './LikeButton';
+import React, {useState} from 'react';
+// import LikeButton from './LikeButton';
 
 const Article = (props) => {
+    const [isPublished, togglePublished] = useState(false);
+
     let publishState = "";
-    if(props.isPublished) {
+    if(isPublished) {
         publishState = "公開"
     } else {
         publishState = "非公開"
@@ -12,9 +14,9 @@ const Article = (props) => {
         <div>
             <h2>{props.title}</h2>
             <label htmlFor="check">公開状態：</label>
-            <input type="checkbox" checkd={props.isPublished} id="check" onClick={() => props.toggle()}/>
+            <input type="checkbox" checkd={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
             <p>{publishState}</p>
-            <LikeButton count={props.count} />
+            {/* <LikeButton count={props.count} /> */}
         </div>
     )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,55 +2,39 @@ import React from 'react';
 import Article from './Article';
 
 
-class Blog extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isPublished: false,
-            count: 0
-        }
-    }
+const Blog = () => {
+    // componentDidMount() {
+    //     //ボタンがクリックされたらいいねをカウントアップする
+    //     document.getElementById('counter').addEventListener('click', this.countUp)
+    // }
 
-    componentDidMount() {
-        //ボタンがクリックされたらいいねをカウントアップする
-        document.getElementById('counter').addEventListener('click', this.countUp)
-    }
+    // componentDidUpdate() {
+    //     if (this.state.count >= 10) {
+    //         this.setState({count: 0})
+    //     }
+    // }
 
-    componentDidUpdate() {
-        if (this.state.count >= 10) {
-            this.setState({count: 0})
-        }
-    }
-
-    componentWillUnmount() {
-        document.getElementById('counter').removeEventListener('click', this.countUp)
-    }
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp)
+    // }
 
     //公開状態を反転させる関数
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-    };
 
-    countUp = () => {
-        this.setState({ count: this.state.count + 1})
-    }
 
-    render() {
-        return(
-            <React.Fragment>
-                <Article 
-                    title="トラハックをよろしく"
-                    //同コンポーネント内はthis.state.key名で渡す
-                    isPublished={this.state.isPublished}
-                    //関数式に関して渡すことによって無限ループを回避、受け取る側も関数式にする
-                    toggle={() => this.togglePublished()}
-                    count={this.state.count}
-                />
-            </React.Fragment>
-        )
-    }
+    // countUp = () => {
+    //     this.setState({ count: this.state.count + 1})
+    // }
+
+
+    return(
+        <React.Fragment>
+            <Article 
+                title="トラハックをよろしく"
+                // count={this.state.count}
+            />
+        </React.Fragment>
+    )
 }
+
 
 export default Blog
